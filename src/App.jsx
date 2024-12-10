8b156d6124c6900dae45226919aa487779a1c184
Xandr <ravnb1989@gmail.com>
2024-12-10 15:06:02 +0200

    profile 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@ import Profile from './components/Profile/Profile';
 import FriendList from './components/FriendList/FriendList';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
-const App = () => {
+import userData from './userData.json';
+
+
+const App = () => { 
     return (
         <>
-            <Profile />
+            <Profile
+                name={userData.username}
+                tag={userData.tag}
+                location={userData.location}
+                image={userData.avatar}
+                stats={userData.stats}
+            />
             <FriendList />
             <TransactionHistory />
         </>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,31 @@
-const Profile = () => {
+const Profile = ({name, tag, location, image, stats}) => {   
     return (
         <>
-            <div>Under Construction 1</div>
+            <div>
+                <div>
+                    <img
+                    src={image}
+                    alt="User avatar"
+                    />
+                    <p>{name}</p>
+                    <p>@{tag}</p>
+                    <p>{location}</p>
+                </div>
+                <ul>
+                    <li>
+                        <span>Followers</span>
+                        <span>{stats.followers}</span>
+                    </li>
+                    <li>
+                        <span>Views</span>
+                        <span>{stats.views}</span>
+                    </li>
+                    <li>
+                        <span>Likes</span>
+                        <span>{stats.likes}</span>
+                    </li>
+                </ul>
+            </div>
         </>
     )
 }
